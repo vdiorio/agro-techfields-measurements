84efa0b6094a3a298c908f638918de26a1ebb451
Vitor Diorio <vitor.diorio@yahoo.com.br>
2022-07-11 13:53:11 -0300

    chore: Setup inicial do Spring

---
+++ backend/src/main/java/com/posthumous/measureshelter/MeasureShelterApplication.java
@@ -0,0 +1,13 @@
+package com.posthumous.measureshelter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeasureShelterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeasureShelterApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/posthumous/measureshelter/MeasureShelterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.posthumous.measureshelter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeasureShelterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

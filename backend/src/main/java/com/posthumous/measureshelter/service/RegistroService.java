0399e87d0e12bd800891f0914ab2a06783919bf8
Vitor Diorio <vitor.diorio@yahoo.com.br>
2022-07-11 18:23:22 -0300

    feat(Service): Criação da entidade service para Registros

---
+++ backend/src/main/java/com/posthumous/measureshelter/service/RegistroService.java
@@ -0,0 +1,16 @@
+package com.posthumous.measureshelter.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.posthumous.measureshelter.model.RegistroIlha;
+import com.posthumous.measureshelter.repository.RegistroRepository;
+
+@Service
+public class RegistroService {
+  @Autowired
+  private RegistroRepository repository;
+
+}

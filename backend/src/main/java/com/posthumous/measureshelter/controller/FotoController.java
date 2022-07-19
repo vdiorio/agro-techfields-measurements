70e9bfdd251d128176fced5109e4e15d5c626480
Vitor Diorio <vitor.diorio@yahoo.com.br>
2022-07-19 19:52:24 -0300

    feat(controller): Rota para upload de fotos

--- backend/src/main/java/com/posthumous/measureshelter/controller/FotoController.java
+++ backend/src/main/java/com/posthumous/measureshelter/controller/FotoController.java
@@ -1,17 +1,25 @@
 package com.posthumous.measureshelter.controller;
 
+import java.io.IOException;
 import java.util.List;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
+import com.posthumous.measureshelter.DTO.FotoDTO;
 import com.posthumous.measureshelter.model.FotoSatelite;
 import com.posthumous.measureshelter.service.FotoService;
 
@@ -21,9 +29,13 @@ public class FotoController {
   private FotoService fotoService;
 
   @PostMapping("/fotos")
-  public ResponseEntity<FotoSatelite> create(@RequestBody FotoSatelite fotoSatelite) {
-    FotoSatelite created = fotoService.create(fotoSatelite);
-    return ResponseEntity.status(HttpStatus.CREATED).body(created);
+  public ResponseEntity<FotoDTO> create(
+      @RequestParam MultipartFile foto,
+      HttpServletRequest request
+    ) throws IOException {
+    FotoSatelite info = fotoService.saveImage(foto);
+      return ResponseEntity.status(HttpStatus.CREATED)
+        .body(new FotoDTO(info, request.getRequestURL().toString()));
   }
 
   @GetMapping("/fotos")

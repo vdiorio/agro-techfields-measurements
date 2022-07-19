1b334152274f730c4e1395ae8f3aea4db1245c47
Vitor Diorio <vitor.diorio@yahoo.com.br>
2022-07-19 19:57:58 -0300

    chore: Adicionando handler para exceção de upload ou download

--- backend/src/main/java/com/posthumous/measureshelter/controller/ErrorHandlers.java
+++ backend/src/main/java/com/posthumous/measureshelter/controller/ErrorHandlers.java
@@ -1,22 +1,29 @@
 package com.posthumous.measureshelter.controller;
 
-import com.posthumous.measureshelter.model.ResponseError;
+import java.io.IOException;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import com.posthumous.measureshelter.DTO.ErrorDTO;
+
 @ControllerAdvice
 public class ErrorHandlers {
   @ExceptionHandler(IllegalArgumentException.class)
-  public ResponseEntity<ResponseError> handleIllegalArgumentException(IllegalArgumentException e) {
-    return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new ResponseError(e.getMessage()));
+  public ResponseEntity<ErrorDTO> handleIllegalArgumentException(IllegalArgumentException e) {
+    return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new ErrorDTO(e.getMessage()));
+  }
+
+  @ExceptionHandler(IOException.class)
+  public ResponseEntity<ErrorDTO> handleIOException(IOException e) {
+    return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(new ErrorDTO(e.getMessage()));
   }
 
   @ExceptionHandler(Exception.class)
-  public ResponseEntity<ResponseError> handleException(Exception e) {
+  public ResponseEntity<ErrorDTO> handleException(Exception e) {
     return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
-        .body(new ResponseError(e.getMessage()));
+        .body(new ErrorDTO(e.getMessage()));
   }
 }
